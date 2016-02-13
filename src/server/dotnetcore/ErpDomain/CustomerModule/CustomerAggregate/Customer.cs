06810670d44b6bb15e82044f024f62777e2dc02a
Harry Pfleger <harry@ardimedia.com>
2016-02-13 11:10:46 +0100

    Baby step... figuring out Domain implementation details.

---
+++ src/server/dotnetcore/ErpDomain/CustomerModule/CustomerAggregate/Customer.cs
@@ -0,0 +1,7 @@
+namespace DevxSample.ErpDomain.CustomerModule.CustomerAggregate
+{
+    public class Customer
+    {
+        public int CustomerPkId { get; set; }
+    }
+}
---
+++ src/server/nodejs/ErpDomain/CustomerModule/CustomerAggregate/Customer.ts
@@ -0,0 +1,16 @@
+module DevxSample.ErpDomain.CustomerModule.CustomerAggregate {
+    /**
+     * Descripts a Customer entity.
+     */
+    export class Customer {
+        /**
+         * @param firstname The first name of the customer.
+         */
+        constructor(firstname: string)
+        {
+            
+        }
+    }
+    
+    var x = new Customer('Harry')
+}
